c1488fe09f3595d5e0e201bbafa935514dad8e82
cob16 <public+github@cormacbrady.info>
2020-02-19 17:25:25 +0000

    setup a vpc and subnets

--- pairing/main.tf
+++ pairing/main.tf
@@ -17,6 +17,34 @@ module "label" {
   delimiter  = "-"
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "~> v2.0"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  tags = module.label.tags
+}
+
+
+output "VPC_ID" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
 
 resource "aws_iam_group" "pairing" {
   name = module.label.id
